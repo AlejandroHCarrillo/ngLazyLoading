fb13b83fc9c4d868b959d18768ac6bde2824ce14
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-03-30 02:00:53 -0600

    Inital commit

---
+++ src/components/common/side-bar-right/side-bar-right.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-right',
+  imports: [],
+  templateUrl: './side-bar-right.component.html',
+  styleUrl: './side-bar-right.component.css'
+})
+export class SideBarRightComponent {
+
+}
